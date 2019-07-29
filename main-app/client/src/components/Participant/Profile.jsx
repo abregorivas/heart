e79624409109059600a6e3315cbd7affe271d80d
abregorivas <abregorivas@gmail.com>
2019-07-29 00:12:51 -0700

    refactor Profile comp with new useFetch

--- main-app/client/src/components/Participant/Profile.jsx
+++ main-app/client/src/components/Participant/Profile.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Card from './components/Card'
 import Loader from '../UI/Loader'
 import Error from '../UI/Error'
-import Citations from './components/Citations'
+import { Citations } from './Citations'
 import Notes from './components/Notes'
 import Status from './components/Status'
 import { useFetch } from '../../hooks/useFetch'
@@ -12,22 +12,21 @@ import { useFetch } from '../../hooks/useFetch'
 const useStyles = makeStyles(theme => ({
   root: { padding: theme.spacing(2), backgroundColor: '#f3f7fc' },
   link: {
-    color: "#252525",
+    color: '#252525',
     cursor: 'pointer',
     textTransform: 'uppercase',
     fontSize: 12,
     fontWeight: 600,
     '&:hover': {
       color: '#428FE2',
-      textDecoration: 'none'
+      textDecoration: 'none',
     },
   },
-    arrowLeft: {
-      display: 'inline-block',
-      marginBottom: theme.spacing(1),
-      marginLeft: theme.spacing(1)
-    },
-
+  arrowLeft: {
+    display: 'inline-block',
+    marginBottom: theme.spacing(1),
+    marginLeft: theme.spacing(1),
+  },
 }))
 
 /*
@@ -37,33 +36,34 @@ Also removed sass requirement and using material-ui grid
  */
 
 const ParticipantProfile = props => {
-  const [state, fetchData, dispatch] = useFetch(
+  const [{ data, isLoading, isError }, fetchData] = useFetch(
     `participants/${props.match.params.id}`
   )
   const classes = useStyles()
+  const user = data[0]
   return (
     <div className={classes.root}>
-      {state.isLoading ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <Container>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Link to="/participants" className={classes.link}>
-                {state.isError && <Error error={state.isError} />}
+                {isError && <Error error={isError} />}
                 <i className="fas fa-arrow-left"></i>
                 <span className={classes.arrowLeft}>Back to Index</span>
               </Link>
             </Grid>
             <Grid item xs={12}>
-              <Card user={state.data[0]} />
+              <Card user={user} />
             </Grid>
             <Grid item xs={8}>
-              <Notes user={state.data[0]} />
-              <Citations user={state.data[0]} />
+              <Notes user={user} />
+              <Citations userId={user.id} />
             </Grid>
             <Grid item xs={4}>
-              <Status user={state.data[0]} />
+              <Status user={user} />
             </Grid>
           </Grid>
         </Container>
